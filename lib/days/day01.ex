e51722468be36dfa4388fa59339104db0b257fb7
Steve Rowley <srowley@gmail.com>
2019-12-02 02:33:00 +0000

    day 1 part 2

--- lib/days/day01.ex
+++ lib/days/day01.ex
@@ -4,11 +4,18 @@ defmodule Day01 do
     |> String.trim()
     |> String.split()
     |> Stream.map(&String.to_integer/1)
-    |> Stream.map(&module_fuel_requirement/1)
+    |> Stream.map(&fuel_requirement/1)
+    |> Stream.map(&fuel_gross_up/1)      # This line and function implement Part 2
     |> Enum.sum
   end
 
-  defp module_fuel_requirement(module_mass) do
-    div(module_mass, 3) - 2
+  defp fuel_requirement(mass), do: div(mass, 3) - 2
+
+  defp fuel_gross_up(mass), do: fuel_gross_up(mass, mass)
+
+  defp fuel_gross_up(0, acc), do: acc
+  defp fuel_gross_up(mass, acc) do
+    requirement = max(0, fuel_requirement(mass))
+    fuel_gross_up(requirement, acc + requirement)
   end
 end
