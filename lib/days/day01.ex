0c7e7bd7234d5173d8e7bc795062e3af49db17b4
Steve Rowley <srowley@gmail.com>
2019-12-02 01:21:55 +0000

    day 1 part 1

--- lib/days/day01.ex
+++ lib/days/day01.ex
@@ -2,12 +2,13 @@ defmodule Day01 do
   def ship_fuel_requirement(input) do
     input
     |> String.trim()
+    |> String.split()
     |> Stream.map(&String.to_integer/1)
     |> Stream.map(&module_fuel_requirement/1)
     |> Enum.sum
   end
 
   defp module_fuel_requirement(module_mass) do
-    (module_mass / 3) - 2
+    div(module_mass, 3) - 2
   end
 end
